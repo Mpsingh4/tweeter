e4d130641e3edc4d52e43676a622c42ef7c8d4fe
Manpreet <mp_manpreet@hotmail.com>
2023-05-11 07:01:41 +0000

    implement dynamic tweets + submission jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,50 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": new Date().getTime() - (16 * 24 * 60 * 60 * 1000) // 16 days ago
-  },
-  {
-    "user": {
-      "name": "Jerry",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@GrandLineReview"
-    },
-    "content": {
-      "text": "One Piece is the greatest Shonen manga!"
-    },
-    "created_at": new Date().getTime() - (10 * 24 * 60 * 60 * 1000) // 10 days ago
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": new Date().getTime() - (16 * 24 * 60 * 60 * 1000) // 16 days ago
+//   },
+//   {
+//     "user": {
+//       "name": "Jerry",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@GrandLineReview"
+//     },
+//     "content": {
+//       "text": "One Piece is the greatest Shonen manga!"
+//     },
+//     "created_at": new Date().getTime() - (10 * 24 * 60 * 60 * 1000) // 10 days ago
+//   }
+// ];
+
+// submit tweet
+const $submitButton = $('.tweet-button');
+$submitButton.on('click', function(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Serialize data
+  const formData = $('form').serialize();
+  console.log(formData);
+
+  // Send an AJAX request to the server
+  $.ajax({
+    url: '/tweets',
+    method: 'POST',
+    data: formData,
+  }).then(function(response) {
+    console.log(response);
+  });
+});
 
 // Define a function that creates a tweet element using a tweet object
 const createTweetElement = function(tweet) {
@@ -64,3 +84,19 @@ const renderTweets = function(tweets) {
 
 // Call the renderTweets function with a data object as an argument
 renderTweets(data);
+
+
+const loadTweets = function() {
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'GET',
+      success: function(response) {
+        renderTweets(response);
+      },
+      error: function(xhr, status, error) {
+        // Handle any errors 
+        console.log('Error:', error);
+      }
+    });
+    return loadTweets();
+  };
